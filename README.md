9d08f234a5ac307d5566357b46054c4726c8a0a5
Nicki Anselmo <nickianselmo@gmail.com>
2019-04-09 15:38:04 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Charts Example Project
+This is an example of how to grab data from the server and display it on the client side as a Chart.
+This example uses Google's Charts API.
+
+## Running the Example
+1. Clone this repo.
+2. Change directories so that you are in `pulse-check-project`.
+3. Run the server by typing `mvn appengine:devserver` into the terminal
---
+++ src/main/java/com/nicki/codeu/servlets/MainServlet.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nicki.codeu.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** Handles fetching and saving {@link Message} instances. */
+@WebServlet("/mood")
+public class MainServlet extends HttpServlet {
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    String energy_String = request.getParameter("usr_energy");
+    int energy = Integer.parseInt(energy_String);
+    String mood = request.getParameter("usr_mood");
+    response.getWriter().println("a user felt " + mood + " with an energy level of " + energy_String);
+  }
+}
